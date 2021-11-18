e5bc5b92c38f2d30295543afe82d12c8e097015e
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2021-11-18 16:43:09 +0100

    Challenges Refactoring

---
+++ Challenges/03_NumberToStringFormat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03_NumberToStringFormat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            //string[] ones = { "one", "two", "three", "four", "five", "six", " seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen" };
+            //string[] tens = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+            Console.WriteLine("Enter the Number:");
+            //int number = Console.Read();
+            int number = Convert.ToInt32(Console.ReadLine());
+            string result;
+
+
+            Console.WriteLine("Entered number is:");
+
+            /*write down your logic here
+           */
+            result = NumberToStringFormatter.Format(number);
+            Console.WriteLine(result);
+
+
+        }
+    }
+
+    public class NumberToStringFormatter
+    {
+
+
+        public static string Format(int number)
+        {
+            string[] ones = { "one", "two", "three", "four", "five", "six", " seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "forteen", "fifteen", "sixteen", "seventeen", "eighteen", "ninteen" };
+            string[] tens = { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+            string result;
+            if (number < 20)
+                result = ones[number - 1];
+            else
+            {
+
+                int x = number - 20;
+                result = tens[x / 10];
+                if (x % 10 > 0 && x % 10 < 10)
+                    result += " " + ones[x % 10 - 1];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Challenges/08_PrintRectangle/Program.cs
@@ -0,0 +1,65 @@
+﻿//Write a program which takes input length and width of rectangle as integer and print the rectangle.
+
+//    Example:
+//1.If width = 10 and height = 5 then the rectangle is :
+
+//**********
+//    **
+//    **
+//    **
+//**********
+
+//  1.If width = 15 and height = 10 then the rectangle is :
+
+//***************
+//    **
+//    **
+//    **
+//    **
+//    **
+//    **
+//    **
+//    **
+// ***************
+
+using System;
+
+namespace _08_PrintRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, w;
+            Console.WriteLine("Enter the length:");
+            l = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width:");
+            w = int.Parse(Console.ReadLine());
+
+            //Write your logic here
+            WriteStars(l);
+            for (int i = 0; i < w - 2; i++)
+            {
+                Console.Write('*');
+                for (int j = 0; j < l - 2; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('*');
+                Console.WriteLine();
+            }
+            WriteStars(l);
+            Console.ReadLine();
+            //end
+        }
+
+        private static void WriteStars(int w)
+        {
+            for (int i = 0; i < w; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+        }
+    }
+}
