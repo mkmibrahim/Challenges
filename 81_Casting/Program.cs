c206568948d840a50216d22c0f6a9906e6880195
MKMIbrahim <m.ibrahim2@krohne.com>
2023-03-09 17:30:51 +0100

    81_Casting

---
+++ 81_Casting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _81_Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
