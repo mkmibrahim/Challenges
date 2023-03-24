e61bf8eae991f95d98cca8301e7532dc656fc372
MKMIbrahim <m.ibrahim2@krohne.com>
2023-03-24 10:43:42 +0100

    85_Constants

---
+++ 85_Constants/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _85_Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
