92dd3f2b8e6bde38c2344ad12d8ad4496eff3b33
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-04-06 07:51:02 +0200

    92_Ternary

---
+++ 92_Ternary/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _92_Ternary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
