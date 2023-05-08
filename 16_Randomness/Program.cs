b2ada4cf547372aa9a0618b28b58947577fbb001
MKMIbrahim <m.ibrahim2@krohne.com>
2023-05-08 08:29:42 +0200

    16_Randomness

---
+++ 16_Randomness/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _16_Randomness
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Player
+    {
+        Random rnd = new Random();
+        public int RollDie()
+        {
+            return rnd.Next(1,18);
+        }
+
+        public double GenerateSpellStrength()
+        {
+            return rnd.NextDouble() * 100;
+        }
+    }
+
+}
