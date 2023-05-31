fa9b7322d41c917880bf3c52ff396b95afaf0f13
MKMIbrahim <m.ibrahim2@krohne.com>
2023-05-31 09:39:43 +0200

    21_Strings 2

--- 21_Strings.Tests/UnitTest1.cs
+++ 21_Strings.Tests/UnitTest1.cs
@@ -9,5 +9,13 @@
             var expectedResult = "                  Lance Green ♡ Pat Riley                    ";
             Assert.Equal(expectedResult, result);
         }
+
+        [Fact]
+        public void Test2()
+        {
+            string actual = HighSchoolSweethearts.DisplayGermanExchangeStudents("Norbert", "Heidi", new DateTime(2019, 1, 22), 1535.22f);
+            Assert.Equal("Norbert and Heidi have been dating since 22.01.2019 - that's 1.535,22 hours", actual);
+            //Assert.Contains("Norbert and Heidi have been dating since 22.01.2019 - that's ", actual);
+        }
     }
 }
