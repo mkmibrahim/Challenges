4f134addf41feafbcf0952188bb390f6d454db6a
MKMIbrahim <m.ibrahim2@krohne.com>
2023-06-01 08:23:55 +0200

    22_Strings

---
+++ 22_Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("22_Strings.Tests")]
+
+namespace _22_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LogLine.Message("test");
+        }
+    }
+
+    static class LogLine
+    {
+        public static string Message(string logLine) => 
+                        logLine.Substring(logLine.LastIndexOf(':') + 1).Trim();
+
+        public static string LogLevel(string logLine) => 
+                        logLine.Substring(logLine.IndexOf('[') + 1, logLine.IndexOf(']') - 1)
+                          .Trim().ToLower();
+
+        public static string Reformat(string logLine) => 
+                        LogLine.Message(logLine) + " (" + LogLine.LogLevel(logLine) + ")";
+    }
+
+}
