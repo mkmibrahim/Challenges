f10fe38b747ef4ac75417943c624a30b77aab540
MKMIbrahim <m.ibrahim2@krohne.com>
2023-06-26 12:59:48 +0200

    39_HighScores

---
+++ 39_HighScores/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _39_HighScores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class HighScores
+    {
+        List<int> _scores = new List<int>();
+
+        public HighScores(List<int> list) => _scores = list;
+
+        public List<int> Scores() => _scores;
+
+        public int Latest() => _scores.Last();
+
+        public int PersonalBest() => _scores.Max();
+
+        public List<int> PersonalTopThree() =>_scores.OrderByDescending(x => x).Take(3).ToList();
+    }
+
+}
