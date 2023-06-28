74afc55041a77387c65528f7930e0b98d5e1366e
MKMIbrahim <m.ibrahim2@krohne.com>
2023-06-28 13:38:40 +0200

    41_Nucleotide

---
+++ 41_Nucleotide/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _41_Nucleotide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class NucleotideCount
+    {
+        public static IDictionary<char, int> Count(string sequence)
+        {
+            var result = new Dictionary<char, int>
+            {
+                ['A'] = 0,
+                ['C'] = 0,
+                ['G'] = 0,
+                ['T'] = 0
+            };
+            for(int i = 0; i < sequence.Length; i++)
+            {
+                if (result.ContainsKey(sequence[i]))
+                    result[sequence[i]]++;
+                else
+                    throw new ArgumentException();
+            }
+            return result;
+        }
+    }
+}
