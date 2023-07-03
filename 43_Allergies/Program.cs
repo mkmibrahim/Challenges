acc441bb598f560eab28689e66e8920af8c8329c
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-07-03 15:00:08 +0200

    43 Allergies

---
+++ 43_Allergies/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace _43_Allergies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    
+    public enum Allergen
+    {
+        Eggs,
+        Peanuts,
+        Shellfish,
+        Strawberries,
+        Tomatoes,
+        Chocolate,
+        Pollen,
+        Cats
+    }
+
+    public class Allergies
+    {
+        int _allergies;
+
+        public Allergies(int mask)
+        {
+            _allergies = mask;
+        }
+
+        public bool IsAllergicTo(Allergen allergen)
+        {
+            var result = (_allergies & (1 << (int)allergen) ) != 0;
+            return result;
+        }
+
+        public Allergen[] List()
+        {
+            var result = new List<Allergen>();
+            var AllergenValues = Enum.GetValues(typeof(Allergen));
+            foreach (var item in AllergenValues)
+            {
+                if (IsAllergicTo((Allergen)item))
+                    result.Add((Allergen)item);
+            }
+            return result.ToArray();
+        }
+    }
+}
