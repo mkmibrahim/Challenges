9d8a6f47d953073c11eb38e3922f23e24996de47
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-07-11 12:40:47 +0200

    52_Lckadaisical

---
+++ 53_KnowledgeStore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _53_KnowledgeStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
