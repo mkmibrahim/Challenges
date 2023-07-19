740bd54bd584489a469026f3afc13c0dad2b5947
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-07-19 12:52:21 +0200

    58 IndexPush

---
+++ 58_IndexPush/Address.cs
@@ -0,0 +1,24 @@
+﻿﻿using Azure.Search.Documents.Indexes;
+using System;
+
+namespace _58_IndexPush
+{
+    [Serializable]
+    public partial class Address
+    {
+        [SearchableField]
+        public string StreetAddress { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string City { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string StateProvince { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string PostalCode { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string Country { get; set; }
+    }
+}
