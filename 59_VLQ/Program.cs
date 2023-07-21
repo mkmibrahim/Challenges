16bb66617a12fe23de800a4e47a83786d3d53ea0
MKMIbrahim <m.ibrahim2@krohne.com>
2023-07-21 08:57:33 +0200

    59 VLQ

---
+++ 59_VLQ/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _59_VLQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class VariableLengthQuantity
+    {
+        public static uint[] Encode(uint[] numbers)
+        {
+            //return numbers;
+            var result = new List<uint>();
+            foreach (uint number in numbers) {
+                var value = (uint)number;
+                do
+                    {
+                        byte lower7bits = (byte)(value & 0x7f);
+                        value >>= 7;
+                        if (value > 0)
+                            lower7bits |= 129;
+                        result.Add(lower7bits);
+                    } while (value > 0);
+            }
+            return result.ToArray();
+        }
+
+        public static uint[] Decode(uint[] bytes)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
