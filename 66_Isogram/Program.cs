cf5f0964745d6f95396e535db1ebd40c7d3003e2
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-08-07 09:00:48 +0200

    66 Isogram

---
+++ 66_Isogram/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _66_Isogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Isogram
+    {
+        public static bool IsIsogram(string word)
+        {
+            //var letters = new List<char>();
+            //foreach (var letter in word.ToLower())
+            //{
+            //    if (letters.Contains(letter) && Char.IsLetter(letter))
+            //    {
+            //        return false;
+            //    }
+            //    letters.Add(letter);
+            //}
+            //return true;
+            var letters = word.ToLower().Where(Char.IsLetter).ToList();
+            var result = letters.Distinct().Count() == letters.Count();
+            return result;
+        }
+    }
+
+}
