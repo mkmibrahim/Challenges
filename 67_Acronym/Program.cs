7afbed3d6112ec13252b74acac15114c3de356c3
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-08-08 08:43:28 +0200

    67 Acronym

---
+++ 67_Acronym/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _67_Acronym
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Acronym
+    {
+        public static string Abbreviate(string phrase)
+        {
+            var acronym = "";
+            var words = phrase.Split(' ', '-', '_').ToList();
+            words.ForEach(word => {
+                if (word.Length > 0)
+                    acronym += word[0].ToString().ToUpper();
+                });
+            return acronym;
+        }
+    }
+
+}
