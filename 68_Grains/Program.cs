ce829e20ad727b78d620fd586489a62fdce48578
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-08-09 10:14:38 +0200

    68 Grains

---
+++ 68_Grains/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _68_Grains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Grains
+    {
+        public static ulong Square(int n)
+        {
+            if (n < 1 || n > 64)
+                throw new ArgumentOutOfRangeException();
+            else
+            if (n == 1)
+                return 1;
+            else
+                return Square(n - 1) * 2;
+          
+        }
+
+        public static ulong Total()
+        {
+            return (Square(64) - 1)* 2 + 1;
+        }
+    }
+}
