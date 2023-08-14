925c84cd6c5efab0e787e7836f6db99364ab66e1
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-08-14 08:39:29 +0200

    69 Perfect Numbers

---
+++ 69_PerfectNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _69_PerfectNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public enum Classification
+    {
+        Perfect,
+        Abundant,
+        Deficient
+    }
+
+    public static class PerfectNumbers
+    {
+        public static Classification Classify(int number)
+        {
+            if (number < 1)
+                throw new ArgumentOutOfRangeException();
+            var devisors = new List<int>();
+            for (var i = 1; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                    devisors.Add(i);
+            }
+            var sum = devisors.Sum();
+            if (sum == number)
+                return Classification.Perfect;
+            if (sum > number)
+                return Classification.Abundant;
+            return Classification.Deficient;
+
+        }
+    }
+}
