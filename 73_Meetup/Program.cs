0de9775965708691990cfb93b577ff66036b5d45
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-09-01 08:58:19 +0200

    73 Meetup

---
+++ 73_Meetup/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _73_Meetup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    
+
+    public enum Schedule
+    {
+        Teenth,
+        First,
+        Second,
+        Third,
+        Fourth,
+        Last
+    }
+
+    public class Meetup
+    {
+        public Meetup(int month, int year)
+        {
+        }
+
+        public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
