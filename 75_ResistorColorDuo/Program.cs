6ec3dc77a96a59d11719b295a7f57fd44d3be621
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-09-18 15:31:26 +0200

    75 ResistorColorDuo

---
+++ 75_ResistorColorDuo/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _75_ResistorColorDuo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class ResistorColorDuo
+    {
+        public static int Value(string[] colors)
+        {
+            var result = "";
+            foreach (var color in colors)
+            {
+                if (result.Length >= 2)
+                    break;
+                result += GetValue(color);
+            }
+            var intResult = int.Parse(result);
+            return intResult;
+        }
+
+        private static int GetValue(string color) => (color.ToLower()) switch
+        {
+            "black" => 0,
+            "brown" => 1,
+            "red" => 2,
+            "orange" => 3,
+            "yellow" => 4,
+            "green" => 5,
+            "blue" => 6,
+            "violet" => 7,
+            "grey" => 8,
+            "white" => 9,
+            _ => -1
+        };
+    }
+}
