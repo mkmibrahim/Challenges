fc878bb778fcdf014d629646b2b13329d5b971a4
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-09-20 08:33:31 +0200

    ResistorColorDuo_2

---
+++ 76_ResistorColorDuo_2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _76_ResistorColorDuo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class ResistorColorTrio
+    {
+        public static string Label(string[] colors)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+
+}
