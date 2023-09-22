6a056d6bbeef1402e8e0da718e94be22e0a4c768
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-09-22 10:30:35 +0200

    ResistorColorDuo 2

--- 76_ResistorColorDuo_2/Program.cs
+++ 76_ResistorColorDuo_2/Program.cs
@@ -12,8 +12,43 @@
     {
         public static string Label(string[] colors)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            var result = "";
+            for (int i = 0; i < 2; i++)
+            {
+                result += GetColorNumber(colors[i]);
+            }
+
+            result += getPostfix(colors[2]);
+            return result;
         }
+
+        private static string getPostfix(string color) =>
+            color.ToLower() switch
+            {
+                "black" => " ohms",
+                "brown" => "0 ohms",
+                "red" => " kiloohms",
+                "orange" => " kiloohms",
+                "yellow" => "0 kiloohms",
+                _ => ""
+            };
+
+        private static string GetColorNumber(string color) =>
+            color.ToLower() switch
+            {
+
+                "black" => "",
+                "brown" => "1",
+                "red" => "2",
+                "orange" => "3",
+                "yellow" => "4",
+                "green" => "5",
+                "blue" => "6",
+                "violet" => "7",
+                "grey" => "8",
+                "white" => "9",
+                _ => ""
+            };
     }
 
 }
