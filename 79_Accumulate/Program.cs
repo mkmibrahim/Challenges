afd40a001a0b3de16583d7e8778621038c7ef3e4
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-09-28 15:46:01 +0200

    79 Accumulate

--- 79_Accumulate/Program.cs
+++ 79_Accumulate/Program.cs
@@ -10,11 +10,13 @@
 
     public static class AccumulateExtensions
     {
+
         public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
         {
-            var result = new List<U>();
-
-            return result;
+            foreach (T element in collection)
+            {
+                yield return func(element);
+            }
         }
     }
 }
