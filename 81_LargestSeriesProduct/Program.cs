ca8b74c812ce2c2f3416df02e6397da7e58e5fdf
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-10-13 08:58:41 +0200

    81 LargestSeriesProduct

---
+++ 81_LargestSeriesProduct/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _81_LargestSeriesProduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class LargestSeriesProduct
+    {
+        public static long GetLargestProduct(string digits, int span)
+        {
+            var result = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (Int16.Parse(digits[i].ToString()) < span)
+                    continue;
+                var currentProduct = 1;
+                for (int j = 0; j < span; j++)
+                {
+                    currentProduct *= Int16.Parse(digits[j].ToString());
+                }
+
+                result = result > currentProduct ? result : currentProduct;
+            }
+            return result;
+        }
+    }
+}
