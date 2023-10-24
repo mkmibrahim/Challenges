63d7b4d780ef0a28c93abda97ed179b73409038c
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-10-24 08:45:33 +0200

    82 Pascals Triangle

---
+++ 82_PascalsTriangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace _82_PascalsTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class PascalsTriangle
+    {
+        public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+        {
+            return new List<IEnumerable<int>>();
+        }
+
+    }
+}
