8fb3aff346d10b84443672faa140fa3d79ef7363
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-10-27 08:33:41 +0200

    83 Prime Factors

---
+++ 83_PrimeFactors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _83_PrimeFactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class PrimeFactors
+    {
+        public static long[] Factors(long number)
+        {
+            var result = new List<long>();
+            var devisorsRemaining = true;
+            var currentdevisor = 2;
+            while (devisorsRemaining)
+            {
+                if (currentdevisor > number)
+                    devisorsRemaining = false;
+                if (number % currentdevisor == 0)
+                {
+                    result.Add(currentdevisor);
+                }
+                currentdevisor++;
+            }
+            
+            return result.ToArray();
+        }
+    }
+}
