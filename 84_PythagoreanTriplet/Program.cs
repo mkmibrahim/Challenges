b2b92af1969bb3740d1fc7f92f4ccb05cb98f5e9
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-10-31 09:00:08 +0100

    84 Pythagorean Triplet

---
+++ 84_PythagoreanTriplet/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _84_PythagoreanTriplet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class PythagoreanTriplet
+    {
+        public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+        {
+            var result = new List<(int, int, int)>();
+            for (var i = 1; i < sum/2; i++)
+            {
+                for (var j = i; j < sum; j++)
+                {
+                    for (var k = j; k < sum; k++)
+                    {
+                        if (i + j + k == sum &&
+                            i < j && j < k &&
+                            Math.Pow(i, 2) + Math.Pow(j, 2) == Math.Pow(k, 2) 
+                             
+                            )
+                        {
+                            result.Add((i, j, k));
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
