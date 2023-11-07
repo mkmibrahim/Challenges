fe878ec19454f5a6067cfbbde0a8e84edf047f84
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-11-07 13:20:48 +0100

    87 ArmstrongNumbers

---
+++ 87_ArmstrongNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _87_ArmstrongNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class ArmstrongNumbers
+    {
+        public static bool IsArmstrongNumber(int number)
+        {
+            double currentSum = 0;
+            for (int i = 0; i < number.ToString().Length; i++)
+            {
+                char digitChar = number.ToString()[i];
+                var digit = int.Parse(digitChar.ToString());
+                currentSum += Math.Pow(digit, number.ToString().Length);
+            }
+            return currentSum == number;
+        }
+
+    }
+}
