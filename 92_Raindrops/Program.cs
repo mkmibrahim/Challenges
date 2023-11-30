414e6724bf7c1db3d1eea84964006e7e436941f9
mkmibrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-11-30 13:27:29 +0100

    92 Raindrops

---
+++ 92_Raindrops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace _92_Raindrops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Raindrops
+    {
+        public static string Convert(int number)
+        {
+            var result = new StringBuilder();
+            if (number % 3 == 0)
+            {
+                result.Append("Pling");
+            }
+            if (number % 5 == 0)
+            {
+                result.Append("Plang");
+            }
+            if (number % 7 == 0)
+            {
+                result.Append("Plong");
+            }
+            if (result.Length == 0)
+            {
+                result.Append(number.ToString());
+            }
+            return result.ToString();
+        }
+    }
+}
