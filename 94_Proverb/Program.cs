5cabc3f7597a14d28d662529b86fc98fd8d8b8ec
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-12-04 14:05:01 +0100

    94 Proverb

---
+++ 94_Proverb/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _94_Proverb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Proverb
+    {
+        public static string[] Recite(string[] subjects)
+        {
+            var result = new List<string>();
+            if (subjects.Length == 0)
+            {
+                return result.ToArray();
+            }
+            foreach (var subject in subjects)
+            {
+                var index = Array.IndexOf(subjects, subject);
+                if (index == subjects.Length - 1)
+                {
+                    result.Add($"And all for the want of a {subjects[0]}.");
+                }
+                else
+                {
+                    result.Add($"For want of a {subject} the {subjects[index + 1]} was lost.");
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
