40d89266b6dbefaab220a21933e53f7eb146ea22
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2023-12-05 10:15:56 +0100

    95 Strain

---
+++ 95_Strain/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _95_Strain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Strain
+    {
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)=>
+            collection.Where(predicate);
+        
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate) =>
+            collection.Where(x => !predicate(x));
+    }
+}
